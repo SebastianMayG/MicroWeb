a7b415bfbe6d29fb16a994c7d090866a70034d76
Luis Fernando Reyes Betancourt <150478475+FernandoReyes04@users.noreply.github.com>
2024-03-19 18:47:07 -0600

    se creó ingreso, detalle ingreso, ingresoFormRequest e ingresoController (18,19,20)
    se creó ingreso, detalle ingreso, ingresoFormRequest e ingresoController (18,19,20)

---
+++ Pagina/app/Http/Requests/IngresoFormRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class IngresoFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'idproveedor'=>'required',
+            'tipo_comprobante'=>'required|max:20',
+            'serie_comprobante'=>'max:7',
+            'num_comprobante'=>'required|max:10',
+            'idarticulo'=>'required',
+            'cantidad'=>'required',
+            'precio_compra'=>'required',
+            'precio_venta'=>'required'
+        ];
+    }
+}
