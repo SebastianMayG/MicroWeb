e8225ed2086dba1c9d366c8058516de4f489ee5b
Luis Fernando Reyes Betancourt <150478475+FernandoReyes04@users.noreply.github.com>
2024-03-19 18:52:15 -0600

    se creó ingreso, detalle ingreso, ingresoFormRequest e ingresoController (18,19,20)

--- Pagina/app/Models/Ingreso.php
+++ Pagina/app/Models/Ingreso.php
@@ -7,5 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ingreso extends Model
 {
-    use HasFactory;
+    protected $table='ingreso';
+
+    protected $primaryKey='idingreso';
+
+    public $timestamps=false;
+
+    protected $fillable =[
+     'idproveedor',
+     'tipo_comprobante',
+     'serie_comprobante',
+     'num_comprobante',
+     'fecha_hora',
+     'impuesto',
+     'estado'
+    ];
+    protected $guarded =[
+    ];
 }
