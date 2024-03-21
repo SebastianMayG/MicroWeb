95bf8eb9d802739181a3de772da8b0ad033a0a39
SebastianMayG <al064488@uacam.mx>
2024-03-20 23:17:03 -0600

    correcion min16.56 video 24

--- Pagina/resources/views/compras/ingreso/create.blade.php
+++ Pagina/resources/views/compras/ingreso/create.blade.php
@@ -135,28 +135,29 @@
 	var subtotal =[];
 	$('#guardar').hide();
 
-	function agregar(){
-    idarticulo=$("#pidarticulo").val();
-    articulo=$("#pidarticulo option:selected").text();
-    cantidad=$("#pcantidad").val();
-    precio_compra=$("#pprecio_compra").val();
-    precio_venta=$("#pprecio_venta").val();
+	function agregar()
+	{
+		idarticulo=$("#pidarticulo").val();
+		articulo=$("#pidarticulo option:selected").text();
+		cantidad=$("#pcantidad").val();
+		precio_compra=$("#pprecio_compra").val();
+		precio_venta=$("#pprecio_venta").val();
 
-    if (idarticulo!="" && cantidad!="" && cantidad>0 && precio_compra!="" && precio_venta!="")
-    {
-        subtotal[cont]=(cantidad*precio_compra);
-        total=total+subtotal[cont];
+		if (idarticulo!="" && cantidad!="" && cantidad>0 && precio_compra!="" && precio_venta!="")
+		{
+			subtotal[cont]=(cantidad*precio_compra);
+			total=total+subtotal[cont];
 
-        var fila='<tr class="selected" id="fila'+cont+'"><td><button type="button" class="btn btn-warning" onclick="eliminar('+cont+');">X</button></td><td><input type="hidden" name="idarticulo[]" value="'+idarticulo+'">'+articulo+'</td><td><input type="number" name="cantidad[]" value="'+cantidad+'"></td><td><input type="number" name="precio_venta[]" value="'+precio_venta+'"></td><td>'+subtotal[cont]+'</td></tr>';
-        cont++;
-        limpiar();
-        $("#total").html("MXN/" + total);
-        evaluar();
-        $("#detalles").append(fila);
-    } else {
-        alert("Error al ingresar el detalle de ingreso, revise los datos del artículo");
-    }
-}
+			var fila='<tr class="selected" id="fila'+cont+'"><td><button type="button" class="btn btn-warning" onclick="eliminar('+cont+');">X</button></td><td><input type="hidden" name="idarticulo[]" value="'+idarticulo+'">'+articulo+'</td><td><input type="number" name="cantidad[]" value="'+cantidad+'"></td><td><input type="number" name="precio_compra[]" value="'+precio_compra+'"></td><td><input type="number" name="precio_venta[]" value="'+precio_venta+'"></td><td>'+subtotal[cont]+'</td></tr>';
+			cont++;
+			limpiar();
+			$("#total").html("MXN/" + total);
+			evaluar();
+			$("#detalles").append(fila);
+		} else {
+			alert("Error al ingresar el detalle de ingreso, revise los datos del artículo");
+		}
+	}
 
 
 	function limpiar() {
