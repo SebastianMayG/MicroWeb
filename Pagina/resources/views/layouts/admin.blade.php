db9b1ca64c1e13910c6922da4e8013b0ea340075
Fernando G <al064504@uacam.mx>
2024-03-24 21:47:42 -0600

    Correción de rutas

--- Pagina/resources/views/layouts/admin.blade.php
+++ Pagina/resources/views/layouts/admin.blade.php
@@ -173,7 +173,7 @@
                 <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li><a href="organizador/calendario"><i class="fa fa-circle-o"></i>Calendario</a></li>
+                <li><a href="/organizador/calendario"><i class="fa fa-circle-o"></i>Calendario</a></li>
               </ul>
             </li>
 
