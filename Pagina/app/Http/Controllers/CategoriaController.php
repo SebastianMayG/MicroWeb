53efb2dc1b3d4399cbed4f986883ea4c8dd108a3
Luis Fernando Reyes Betancourt <150478475+FernandoReyes04@users.noreply.github.com>
2024-03-27 11:13:15 -0600

    Revert "Login y gestion de usuarios"
    This reverts commit 13b5975970c4d89fbd5356b0182b5ac898bebc6f.

--- Pagina/app/Http/Controllers/CategoriaController.php
+++ Pagina/app/Http/Controllers/CategoriaController.php
@@ -12,7 +12,6 @@ class CategoriaController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
     }
 
     public function index(Request $request)
--- Pagina/resources/views/auth/register.blade.php
+++ Pagina/resources/views/auth/register.blade.php
@@ -1,77 +1,92 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+    <!-- CSS local -->
+    <link rel="stylesheet" href="{{ asset('assets/estilos.css') }}">
+</head>
 
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+    <body>
+    <section class="h-100 gradient-form" style="background-color: #eee;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-xl-5">
+        <div class="card rounded-3 text-black">
+          <div class="row g-0">
+            <div class="col-lg-12">
+              <div class="card-body p-md-3 mx-md-4">
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                <div class="text-center">
+                  <img src="{{asset ('assets/LogoGris.jpeg')}}"
+                    style="width: 110px;" alt="logo">
+                  <h4 class="mt-1 mb-1 pb-1">MicroWeb</h4>
+                  <h5 class="mt-1 mb-3 pb-1">¿Listo para crecer?</h5>
                 </div>
+
+                <form action="{{route('register')}}" method="post">
+                    @csrf
+
+                  <div class="form-outline mb-4">
+                    <input type="text" name= "name" id="form2Example11" class="form-control"
+                      placeholder="Ingrese nombre" />
+                    <label class="form-label" for="form2Example11">Nombre</label>
+                  </div>
+
+                  <div class="form-outline mb-4">
+                    <input type="email" name= "email" id="form2Example11" class="form-control"
+                      placeholder="Teléfono o Correo electrónico" />
+                    <label class="form-label" for="form2Example11">Correo</label>
+                  </div>
+
+                  <div class="form-outline mb-4">
+                    <input type="password" name="password" id="form2Example22" class="form-control" />
+                    <label class="form-label" for="form2Example22">Contraseña</label>
+                  </div>
+
+                  <div class="form-outline mb-4">
+                    <input type="password" name="password_confirmation" id="form2Example22" class="form-control" />
+                    <label class="form-label" for="form2Example22">Confirmar contraseña</label>
+                  </div>
+
+                  <div class="text-center pt-1 mb-2 pb-1">
+                    <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="submit">Registrarse</button>
+                  </div>
+
+                  <div class="d-flex align-items-center justify-content-center pb-2">
+                    <a href="{{route('login')}}" class="btn btn-outline-danger">Iniciar sesión</a>
+                  </div>
+
+                </form>
+
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
-@endsection
+  </div>
+</section>    
+
+    
+
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
