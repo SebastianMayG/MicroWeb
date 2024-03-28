4e4a467152d3d92436b65adfd013e7f1eff6ddb7
SebastianMayG <al064488@uacam.mx>
2024-03-28 13:10:29 -0600

    Acomodo de las graficas de ventas

--- Pagina/resources/views/ventas/graficas/index.blade.php
+++ Pagina/resources/views/ventas/graficas/index.blade.php
@@ -1,32 +1,47 @@
 @extends ('layouts.admin')
 @section ('contenido')
+<style>
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    .card {
+    background-color: #fff;
+    max-width: 600px;
+    margin: 20px auto;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    text-align: center;
+    }
+    
+</style>
+<div id="asesores">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <div class="card">
+                    <div class="card-body">
 
-                <div class="card-body">
+                        <div class="chart-container" style="width: 400px; height: 450px;">
+                            <h2>{{ $chart->options['chart_title'] }}</h2>
+                            {!! $chart->renderHtml() !!}
+                        </div>
 
-                    <div class="chart-container" style="width: 400px; height: 300px;">
-                        <h2>{{ $chart->options['chart_title'] }}</h2>
-                        {!! $chart->renderHtml() !!}
                     </div>
-                    <div class="chart-container" style="width: 200px; height: 200px;">
-                    </div>
-                    <div class="chart-container" style="width: 400px; height: 300px;">
-                        <h2>{{ $chart1->options['chart_title'] }}</h2>
-                        {!! $chart1->renderHtml() !!}
-                    </div>
-
                 </div>
-
             </div>
-        </div> 
-    </div>
-</div>
 
+            <div class="col-md-5">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="chart-container" style="width: 400px; height: 300px;">
+                            <h2>{{ $chart1->options['chart_title'] }}</h2>
+                            {!! $chart1->renderHtml() !!}
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        </div>
+    </div> 
+</div>
 @endsection
 
 @section('scripts')
