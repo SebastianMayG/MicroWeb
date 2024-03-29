48aa44e3be0bf698313ae86790e8311c914ff47a
Luis Fernando Reyes Betancourt <150478475+FernandoReyes04@users.noreply.github.com>
2024-03-28 18:20:40 -0600

    ChatBot

--- Pagina/resources/views/PlanNegocio/index.blade.php
+++ Pagina/resources/views/PlanNegocio/index.blade.php
@@ -1,4 +1,22 @@
+<!-- Tu vista -->
 @extends ('layouts.admin')
+
 @section ('contenido')
     <h1>Mi Plan de Negocio</h1>
+@endsection
+
+@section ('scripts')
+<script type="text/javascript">
+  (function(d, t) {
+      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+      v.onload = function() {
+        window.voiceflow.chat.load({
+          verify: { projectID: '6605d9bbe773c6d866e27b02' },
+          url: 'https://general-runtime.voiceflow.com',
+          versionID: 'production'
+        });
+      }
+      v.src = "https://cdn.voiceflow.com/widget/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+  })(document, 'script');
+</script>
 @endsection
