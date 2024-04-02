4546b87787b0be93b5f8d4e91bd69673c2d68f27
SebastianMayG <al064488@uacam.mx>
2024-04-02 14:47:43 -0600

    Change index cursos

--- Pagina/resources/views/TipsYCursos/Cursos/index.blade.php
+++ Pagina/resources/views/TipsYCursos/Cursos/index.blade.php
@@ -37,7 +37,15 @@
           background-color: #0056b3;
           cursor: pointer;
       }
+
+  #center {
+    text-align: center;
+    display: block;
+  }
+
 </style>
+
+<h1 id="center">Cursos</h1>
   <div id="cursos">
         <div class="card">
           <img class="cursos" src="https://mipymes.economia.gob.mx/wp-content/uploads/2022/05/HEADER_BannerMIPYMES-MX.png" alt="Imagen del curso" width="200">
@@ -64,10 +72,12 @@
           <a href="https://www.youtube.com/watch?v=iSNMvzR8-8U&list=PLzsP80HPQJ61cvyNvChaeaQOuEooCstlc" class="Reservar" target="_blank">Ir</a>
         </div>
       </div>
-      <div class="card">
-          <h2>Coppel Emprende</h2>
+      <div id="center">
+          <h1>Coppel Emprende</h1>
+          <iframe width="800" height="450" src="https://www.fundacioncoppel.org/wp-content/uploads/2024/02/coppel-comofuncionaenko.mp4"title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           <br></br>
-          <a href="https://www.enko.org/mx/encoppel/inicio" class="Reservar" target="_blank">Ir</a>
+          <div><a style="margin: 10px;" href="https://www.enko.org/mx/encoppel/inicio" class="Reservar" target="_blank">Ir</a></div>
+          <br></br>
       </div>
   </div> 
 @endsection
